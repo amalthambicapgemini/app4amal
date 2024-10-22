0bf158ff846880d73f4e964143bd1d931f43e2c1
Amal Thambi Capgemini <amal.thambi@capgemini.com>
2024-10-22 22:50:41 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Welcome to the world of SRE Masterclass by Amal LnD Capgemini</h3>
+    </div>
+  );
+}
+
+export default App;
